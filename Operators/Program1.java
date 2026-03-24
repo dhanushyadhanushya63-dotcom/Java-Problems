3ab7f7f5ba94127de68045b79e80433cf6447284
Dhanushya Muthumani <dhanushyadhanushya63@gmail.com>
2026-03-24 15:28:21 +0530

    Add files via upload

---
+++ Operators/Program1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int theory=sc.nextInt();
+        int practical=sc.nextInt();
+        double avg=(theory+practical)/2.0;
+        if(theory>=50 && practical>=50 && avg>=60){
+            System.out.println("PASS");
+        }else{
+            System.out.println("FAIL");
+        }
+    }
+}
