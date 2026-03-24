26fbccf70f6a52298ccd8f0e275ee9078ad66b92
Dhanushya Muthumani <dhanushyadhanushya63@gmail.com>
2026-03-24 15:29:46 +0530

    Add files via upload

---
+++ Operators/Program2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       int accountBalance=sc.nextInt();
+       int withdrawal=sc.nextInt();
+       int dailyLimit=sc.nextInt();
+       if(withdrawal<=accountBalance && withdrawal<=accountBalance){
+           System.out.println("Approved");
+       }
+       else{
+           System.out.println("Declined");
+       }
+    }
+}
