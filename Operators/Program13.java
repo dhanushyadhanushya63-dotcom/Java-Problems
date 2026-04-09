6307baf3937c8c3d2a361fba7af0eff0b243f2f6
Dhanushya Muthumani <dhanushyadhanushya63@gmail.com>
2026-04-09 19:49:15 +0530

    Add files via upload

---
+++ Operators/Program13.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[]args){
+       Scanner sc=new Scanner(System.in);
+       int connectionValue=sc.nextInt();
+       int applianceMask=sc.nextInt();
+       if((connectionValue & applianceMask)!=0){
+           System.out.println("Active");
+       }else{
+           System.out.println("Inactive");
+       }
+    }
+}
