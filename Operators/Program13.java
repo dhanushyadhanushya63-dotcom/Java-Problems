61f956af896a854c0b62a7d191cc438939080705
Dhanushya Muthumani <dhanushyadhanushya63@gmail.com>
2026-04-10 19:12:24 +0530

    Update Program13.java

--- Operators/Program13.java
+++ Operators/Program13.java
@@ -3,7 +3,7 @@ class Main{
     public static void main(String[]args){
        Scanner sc=new Scanner(System.in);
        int connectionValue=sc.nextInt();
-       int applianceMask=sc.nextInt();
+       int applianceMask=sc.nextInt(); 
        if((connectionValue & applianceMask)!=0){
            System.out.println("Active");
        }else{
