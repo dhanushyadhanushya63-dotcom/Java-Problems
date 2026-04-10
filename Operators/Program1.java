b162a7394eaea70be1a395e70d90b750fdfe5ac2
Dhanushya Muthumani <dhanushyadhanushya63@gmail.com>
2026-04-10 19:11:01 +0530

    Fix formatting issues in Program1.java

--- Operators/Program1.java
+++ Operators/Program1.java
@@ -4,7 +4,7 @@ class Main {
         Scanner sc=new Scanner(System.in);
         int theory=sc.nextInt();
         int practical=sc.nextInt();
-        double avg=(theory+practical)/2.0;
+        double avg=(theory+practical)/2.0; 
         if(theory>=50 && practical>=50 && avg>=60){
             System.out.println("PASS");
         }else{
