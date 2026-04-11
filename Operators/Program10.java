6af31abf57e65c211c9bd98940d01471bf3eeea4
Dhanushya Muthumani <dhanushyadhanushya63@gmail.com>
2026-04-11 20:43:02 +0530

    Update Program10.java

--- Operators/Program10.java
+++ Operators/Program10.java
@@ -3,7 +3,7 @@ class Main{
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
         int unitConsumed=sc.nextInt();
-        int voltageFluctuation=sc.nextInt();
+        int voltageFluctuation=sc.nextInt(); 
         if(unitConsumed>500 || voltageFluctuation==1){
             System.out.println("Alert Triggered");
         }else{
