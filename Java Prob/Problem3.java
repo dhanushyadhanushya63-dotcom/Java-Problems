f25ea42d29bd7629df69549ed095738ad50afd80
Dhanushya Muthumani <dhanushyadhanushya63@gmail.com>
2026-04-12 17:56:19 +0530

    Add files via upload

---
+++ Java Prob/Problem3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num,reverse=0;
+        int remainder;
+        num=sc.nextInt();
+        while(num!=0){
+            remainder=num%10;
+            reverse=reverse*10+remainder;
+            num=num/10;
+        }
+        System.out.println("Reversed Number:"+reverse);
+    }
+}
