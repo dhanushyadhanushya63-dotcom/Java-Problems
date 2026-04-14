efae03463880d50c6c40ddfe960063d5ed4506a6
Dhanushya Muthumani <dhanushyadhanushya63@gmail.com>
2026-04-14 19:50:08 +0530

    Refactor Program2.java for clarity and consistency

--- Program2.java
+++ Program2.java
@@ -3,7 +3,7 @@ class Main {
     public static void main(String[] args) {
        Scanner sc=new Scanner(System.in);
        int accountBalance=sc.nextInt();
-       int withdrawal=sc.nextInt();
+       int withdrawal=sc.nextInt(); 
        int dailyLimit=sc.nextInt();
        if(withdrawal<=accountBalance && withdrawal<=accountBalance){
            System.out.println("Approved");
