153b659684e5016b0c39e74685ef4c027b68abf6
Dhanushya Muthumani <dhanushyadhanushya63@gmail.com>
2026-07-08 09:18:40 +0530

    Add files via upload
    palindrome

--- Program2.java
+++ Program2.java
@@ -1,15 +1,15 @@
-import java.util.Scanner;
-class Main {
-    public static void main(String[] args) {
-       Scanner sc=new Scanner(System.in);
-       int accountBalance=sc.nextInt();
-       int withdrawal=sc.nextInt(); 
-       int dailyLimit=sc.nextInt();
-       if(withdrawal<=accountBalance && withdrawal<=accountBalance){
-           System.out.println("Approved");
-       }
-       else{
-           System.out.println("Declined");
-       }
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int original = x;
+        int reverse = 0;
+        while (x > 0) {
+            int digit = x % 10;
+            reverse = reverse * 10 + digit;
+            x = x / 10;
+        }
+        return original == reverse;
     }
 }
