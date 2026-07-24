c6b012e3a226aca49d5a6881cf1fb9762761c23d
Dhanushya Muthumani <dhanushyadhanushya63@gmail.com>
2026-07-24 13:45:48 +0530

    Update Program3.java

--- Operators/Program3.java
+++ Operators/Program3.java
@@ -3,7 +3,7 @@ class Main {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int workingHours=sc.nextInt();
-        int attendance=sc.nextInt();
+        int attendance=sc.nextInt(); 
        if(workingHours>40 && attendance>90){
            System.out.println("Eligible");
        }else{
