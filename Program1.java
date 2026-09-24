d18a9df8a7bec2727458df6252ff2e099391f2d1
Dhanushya Muthumani <dhanushyadhanushya63@gmail.com>
2026-09-24 10:37:14 +0530

    Update Program1.java

--- Program1.java
+++ Program1.java
@@ -6,7 +6,7 @@ class Main {
         int practical=sc.nextInt();
         double avg=(theory+practical)/2.0; 
         if(theory>=50 && practical>=50 && avg>=60){
-            System.out.println("PASS");
+            System.out.println("PASS"); 
         }else{
             System.out.println("FAIL");
         }
